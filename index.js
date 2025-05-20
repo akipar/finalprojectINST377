d9cdf859b2df3b891e5724450cfaef16e8eab323
akipar <akiparrish@gmail.com>
2025-05-19 22:28:18 -0400

    vercel not working yet

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
 
 const express = require('express');
-const { createServer } = require('@vercel/node');
 const path = require('path');
-
 
 const {createClient} = require('@supabase/supabase-js')
 const bodyParser = require('body-parser');
 
-
+const app=express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
@@ -17,16 +16,6 @@ app.use(express.static(__dirname + '/public'));
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
-
-const app=require('./api/index.js');
-const port = 3000;
-
-
-// app.post('/customer', async(req, res) => {
-//     const {data, error} = await supabase.rpc('increment_visit');
-//     if (error) return res.status(500).json({error});
-//     res.json({count: data});
-// });
 
 
 app.post ('/api/page_views', async (req, res) => {
@@ -37,7 +26,7 @@ app.post ('/api/page_views', async (req, res) => {
         .single();
 
     if (error || !data) {
-        // console.log('retrieval failed', error);
+        console.log('retrieval failed', error);
         return res.status(500).json({error: 'Failed retrieval'})
     }
     const newCount = data.count +1;
@@ -49,11 +38,13 @@ app.post ('/api/page_views', async (req, res) => {
 
     if (updateError) {
         console.log('Update failed', updateError);
-        return res.status(500).json({error: 'Failed update'})
+        return res.status(500).json({error: 'Failed update'});
     }
 
     res.json({count: newCount});
-})
+});
+
+
 
 module.exports = app;
 
